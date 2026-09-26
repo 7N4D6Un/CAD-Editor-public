42175938d5dbe6d822c8a25e8c298260a111575e
DoYmL8c <HSeEnV5@proton.me>
2026-09-27 03:11:13 +0800

    回退联动

--- src/main/java/com/github/rinorsi/cadeditor/client/screen/model/category/item/ItemGeneralCategoryModel.java
+++ src/main/java/com/github/rinorsi/cadeditor/client/screen/model/category/item/ItemGeneralCategoryModel.java
@@ -74,16 +74,8 @@ public class ItemGeneralCategoryModel extends ItemEditorCategoryModel {
         String currentId = BuiltInRegistries.ITEM.getKey(stack.getItem()).toString();
         int currentCount = stack.getCount();
         getEntries().add(new ItemSelectionEntryModel(this, ModTexts.ITEM_ID, currentId, this::setItemId));
-        IntegerEntryModel maxStackSizeEntry = new IntegerEntryModel(this, ModTexts.MAX_STACK_SIZE, getMaxStackSizeValue(stack), value -> setMaxStackSize(value), value -> value.intValue() >= 0 && value.intValue() <= 99);
-        getEntries().add(new IntegerEntryModel(this, ModTexts.COUNT, currentCount, value -> setCount(value), value -> {
-            int max = stack.getMaxStackSize();
-            Integer pendingMax = maxStackSizeEntry.getValue();
-            if (pendingMax != null && pendingMax.intValue() > max) {
-                max = pendingMax.intValue();
-            }
-            return value != null && value >= 1 && value <= max;
-        }));
-        getEntries().add(maxStackSizeEntry);
+        getEntries().add(new IntegerEntryModel(this, ModTexts.COUNT, currentCount, value -> setCount(value), value -> value >= 1 && value <= 99));
+        getEntries().add(new IntegerEntryModel(this, ModTexts.MAX_STACK_SIZE, getMaxStackSizeValue(stack), value -> setMaxStackSize(value), value -> value.intValue() >= 0 && value.intValue() <= 99));
         getEntries().add(new RaritySelectionEntryModel(this, ModTexts.gui("rarity"), getRarityString(stack), this::setRarity));
         ItemSelectionEntryModel itemModelEntry = new ItemSelectionEntryModel(this, ModTexts.gui("item_model"), getItemModelId(stack), this::setItemModelId);
         itemModelEntry.setPlaceholder("minecraft:diamond");
